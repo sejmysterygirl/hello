d2e3d84126cf7a2d998d60871f6fda02c1d32d19
sejmysterygirl <41227514+sejmysterygirl@users.noreply.github.com>
2018-07-14 14:53:49 +0530

    Add files via upload

---
+++ ele bill.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+publuc satic void main(String args[]) throws IOException
+{
+	double rate1=2.5;
+	double rate2=3.0;
+	double rate1=4.5;
+	double rate2=5.0;
+
+	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	
+	Double unit=Double.parsreDouble(br.readLine());
+	
+	if(unit<=100)
